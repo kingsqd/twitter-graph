53d7ab3ce5fc3de39f27f6656c1f349ca923f073
Hakeem King <hakeemking@Hakeems-MacBook-Pro.local>
2018-08-13 23:33:18 -0600

    trouble shoot

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var twitterClient = new Twitter({
 });
 
 var db;
+app.listen(process.env.PORT , '0.0.0.0');
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
     if(err) {
@@ -28,11 +29,11 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     console.log("Database connection ready");
 
     // Initialize app
-    var server = app.listen(process.env.PORT , '0.0.0.0', function(){
-        var port = server.address().port;
-        console.log("App now running on port", port);
-    });
-
+    //var server = app.listen(process.env.PORT , '0.0.0.0', function(){
+      //  var port = server.address().port;
+        //console.log("App now running on port", port);
+   // });
+   //app.listen(process.env.PORT , '0.0.0.0');
 
 });
 
