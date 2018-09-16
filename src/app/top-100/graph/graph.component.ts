dd176c49144a0c97a5a118472b9d0c565bb3e322
Hakeem King <hakeemking@Hakeems-MacBook-Pro.local>
2018-09-16 15:36:40 -0400

    f

--- src/app/top-100/graph/graph.component.ts
+++ src/app/top-100/graph/graph.component.ts
@@ -11,7 +11,9 @@ import { ChartService } from '../chart.service';
 export class GraphComponent implements OnInit {
 
   songs: Songs[]
+  map = new Map<string, number>();
   firstSong: Songs
+  name: number;
   constructor(private chartService: ChartService) { }
 
   ngOnInit() {
@@ -21,9 +23,32 @@ export class GraphComponent implements OnInit {
           this.songs = songs.map((song) => {
             return song;
           })
-          this.firstSong = songs[0];
+          this.populateMap(songs);
+          
         });
+  }
 
+  populateMap(songs: Songs[]): void {
+    songs.forEach(function (song) {
+      let artString = song.artist;
+      artString.replace('Featuring', ',');
+      let i = 0;
+      for(i = 0; i < artString.length; i++){
+          let name = '';
+          while(i < artString.length && (artString.charAt(i) !== ',' || artString.charAt(i) !== '&' || artString.charAt(i) !== '&')){
+            name += artString.charAt(i);
+            i++;
+          }
+          i--;
+          name = name.trim();
+          if(this.map.has(name)){
+            this.map.set(name, this.map.get(name) + 1);
+          } else {
+            this.map.set(name, 1);
+          }
+      }
+    });
+    this.name = this.map.get("Cardi B");
   }
 
 }
